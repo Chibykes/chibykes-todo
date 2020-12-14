fe5cc54a0a3546d032fb7f7dfc04807aff9e5fb6
Chibykes <62060851+Chibykes@users.noreply.github.com>
2020-12-15 00:14:42 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -14,6 +14,4 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(port,()=>{
-    console.log('Server running';)
-});
+app.listen(port);
